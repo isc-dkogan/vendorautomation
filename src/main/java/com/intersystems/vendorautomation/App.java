6da65307870af009209c2455085317899e5bad24
David Kogan <dkogan@usmbp16dkogan2.lan>
2024-10-11 15:32:21 -0400

    successfully called stored procedure

--- src/main/java/com/intersystems/vendorautomation/App.java
+++ src/main/java/com/intersystems/vendorautomation/App.java
@@ -2,6 +2,7 @@ package com.intersystems.vendorautomation;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.CallableStatement;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -13,14 +14,48 @@ public class App {
 
         Connection conn = IrisDatabaseConnection.connectToIrisDatabase(url, username, password);
 
+        App app = new App();
+
+        app.TestStoredProcedure(conn);
+
         if (conn != null) {
             try {
-                // Close the connection after use
                 conn.close();
                 System.out.println("Connection closed.");
             } catch (SQLException e) {
                 System.err.println("Error closing the connection: " + e.getMessage());
             }
         }
+    }
+
+    public void TestStoredProcedure(Connection conn) {
+        CallableStatement stmt = null;
+        String outputValue = null;
+
+        try {
+            String sql = "{CALL SDS_DataLoader.TestStoredProc(?)}";
+            stmt = conn.prepareCall(sql);
+
+            stmt.registerOutParameter(1, java.sql.Types.VARCHAR);
+
+            stmt.execute();
+
+            outputValue = stmt.getString(1);
+
+            System.out.println("Stored procedure returned: " + outputValue);
+        }
+
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        finally {
+            try {
+                if (stmt != null) stmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
