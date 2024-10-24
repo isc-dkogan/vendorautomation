ae42151851205baa270d7d2a9374cd48c1fc4d91
David Kogan <dkogan@usmbp16dkogan2.lan>
2024-10-24 16:45:29 -0400

    import working

--- src/main/java/com/intersystems/vendorautomation/App.java
+++ src/main/java/com/intersystems/vendorautomation/App.java
@@ -1,61 +1,148 @@
 package com.intersystems.vendorautomation;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.CallableStatement;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+
+import com.intersystems.jdbc.IRIS;
+import com.intersystems.jdbc.IRISConnection;
+import com.intersystems.jdbc.IRISDataSource;
+import com.intersystems.jdbc.IRISObject;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Base64;
+import java.util.Set;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
-        String url = "jdbc:IRIS://localhost:41972/B360";
-        String username = "SuperUser";
-        String password = "sys";
-
-        Connection conn = IrisDatabaseConnection.connectToIrisDatabase(url, username, password);
+        IrisDatabaseConnection conn = new IrisDatabaseConnection();
+        IRISDataSource dataSource = conn.createDataSource();
+        IRISConnection connection = (IRISConnection) dataSource.getConnection();
+        IRIS iris = IRIS.createIRIS(connection);
 
         App app = new App();
+        // String newDataSourceId = app.DuplicateSalesforceDataSource(iris, 2, "ISCSalesforcePackage");
 
-        app.TestStoredProcedure(conn);
-
-        if (conn != null) {
-            try {
-                conn.close();
-                System.out.println("Connection closed.");
-            } catch (SQLException e) {
-                System.err.println("Error closing the connection: " + e.getMessage());
-            }
-        }
+        // System.out.println("new data source id: " + newDataSourceId);
+        JSONArray dataSourceItems = app.GetDataSourceItems(5);
+        app.GetDataSchemaDefinitions(iris, 5, dataSourceItems);
     }
 
-    public void TestStoredProcedure(Connection conn) {
-        CallableStatement stmt = null;
-        String outputValue = null;
+    public String DuplicateSalesforceDataSource(IRIS iris, int dataSourceId, String newDataSourceName) {
+        String newDataSourceId = "";
 
         try {
-            String sql = "{CALL SDS_DataLoader.TestStoredProc(?)}";
-            stmt = conn.prepareCall(sql);
+            IRISObject originalDataSource = (IRISObject) iris.classMethodObject("SDS.DataLoader.DS.DataSource", "%OpenId", dataSourceId);
 
-            stmt.registerOutParameter(1, java.sql.Types.VARCHAR);
+            IRISObject newDataSource = (IRISObject) originalDataSource.invoke("%ConstructClone", 0);
 
-            stmt.execute();
+            newDataSource.set("Name", newDataSourceName);
 
-            outputValue = stmt.getString(1);
+            Object sc = newDataSource.invoke("%Save");
 
-            System.out.println("Stored procedure returned: " + outputValue);
+            newDataSourceId = (String) newDataSource.invoke("%Id");
         }
 
-        catch (SQLException e) {
+        catch (Exception e) {
             e.printStackTrace();
         }
 
-        finally {
-            try {
-                if (stmt != null) stmt.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
+        return newDataSourceId;
+
+    }
+
+    public void GetDataSchemaDefinitions(IRIS iris, int dataSourceId, JSONArray itemsArray) {
+
+        try {
+            IRISObject importList = (IRISObject) iris.classMethodObject("intersystems.dataLoader.v1.dataSources.DataSourceImportList", "%New");
+            IRISObject importListItems = (IRISObject) importList.get("items");
+
+            for (int i = 0; i < itemsArray.length(); i++) {
+                JSONObject item = itemsArray.getJSONObject(i);
+                System.out.println("Item " + (i + 1) + ": " + item.toString());
+
+                IRISObject importListItem = (IRISObject) iris.classMethodObject("intersystems.dataLoader.v1.dataSources.DataSourceImportListItem", "%New");
+                importListItem.set("entityName", item.getString("memberName"));
+
+                importListItems.invoke("Insert", importListItem);
             }
+
+            IRISObject dataCatalogService = (IRISObject) iris.classMethodObject("SDS.DataCatalog.BS.Service", "%New", "Data Catalog Service");
+
+            for (int i = 0; i < itemsArray.length(); i++) {
+                JSONObject item = itemsArray.getJSONObject(i);
+
+                IRISObject importRequest = (IRISObject) iris.classMethodObject("SDS.DataCatalog.BO.ImportRequest", "%New");
+                importRequest.set("DataSourceId", dataSourceId);
+                importRequest.set("MemberName", item.getString("memberName"));
+                // importRequest.set("SchemaName", item.getString("schemaName"));
+
+                dataCatalogService.invoke("ProcessInput", importRequest);
+            }
+
         }
+
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public JSONArray GetDataSourceItems(int dataSourceId) {
+
+        JSONArray itemsArray = null;
+
+        try {
+            URL url = new URL("http://localhost:8081/intersystems/data-loader/v1/dataSources/"+dataSourceId+"/schemas/members");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            String userCredentials = "systemadmin:sys";
+            String basicAuth = "Basic " + new String(Base64.getEncoder().encode(userCredentials.getBytes()));
+            connection.setRequestProperty("Authorization", basicAuth);
+
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response Code: " + responseCode);
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuilder content = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    content.append(inputLine);
+                }
+
+                in.close();
+
+                String jsonResponse = content.toString();
+                JSONObject jsonObject = new JSONObject(jsonResponse);
+
+                if (jsonObject.has("items")) {
+                    itemsArray = jsonObject.getJSONArray("items");
+                } else {
+                    System.out.println("No 'items' key found in the response.");
+                }
+            } else {
+                System.out.println("Request failed with response code: " + responseCode);
+            }
+
+            connection.disconnect();
+
+        }
+
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return itemsArray;
 
     }
 }
--- src/main/java/com/intersystems/vendorautomation/IrisDatabaseConnection.java
+++ src/main/java/com/intersystems/vendorautomation/IrisDatabaseConnection.java
@@ -1,29 +1,19 @@
 package com.intersystems.vendorautomation;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+import com.intersystems.jdbc.IRISDataSource;
 
 public class IrisDatabaseConnection {
 
-    // Method to connect to the IRIS database
-    public static Connection connectToIrisDatabase(String url, String username, String password) {
-        Connection connection = null;
-
-        try {
-            // Load the IRIS JDBC driver
-            Class.forName("com.intersystems.jdbc.IRISDriver");
-
-            // Establish the connection
-            connection = DriverManager.getConnection(url, username, password);
-            System.out.println("Connected to IRIS database successfully.");
-
-        } catch (ClassNotFoundException e) {
-            System.err.println("IRIS JDBC Driver not found: " + e.getMessage());
-        } catch (SQLException e) {
-            System.err.println("SQL error occurred: " + e.getMessage());
-        }
-
-        return connection;
-    }
+    public IRISDataSource createDataSource() throws SQLException
+	{
+		IRISDataSource dataSource = new IRISDataSource();
+		dataSource.setServerName("localhost");
+		dataSource.setPortNumber(41972);
+		dataSource.setDatabaseName("B360");
+		dataSource.setUser("SuperUser");
+		dataSource.setPassword("sys");
+		dataSource.setSharedMemory(false);
+		return dataSource;
+	}
 }
